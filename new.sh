fb9da4494c53a60759a87fdc330bc925c9668092
Shivam <sm2552744@gmail.com>
2024-12-14 11:35:26 +0530

    shell;

---
+++ new.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Enter Your Name"
+read name;
+
+echo "Hello, $name"
+
+
+# Sum of two number 
+
+echo "Enter Value to sum two numbers"
+echo "A -"
+read a
+echo "B -"
+read b
+
+echo "Sum is $(($a+$b))"
