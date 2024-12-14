37d12f3ca3bf4fb8548a2c9a35e98a15033af398
Shivam <sm2552744@gmail.com>
2024-12-14 16:30:01 +0530

    shell;

--- conditional.sh
+++ conditional.sh
@@ -1,1 +1,16 @@
-echo "He;;p"
+echo "Age - "
+read age
+
+if [ $age -ge 18 ]; then
+  echo "You are an adult."
+    echo "Do you have voter id"
+
+    read voter
+    if [ $voter -eq 1 ]; then
+        echo "GO to Vote"
+        else
+            echo "Get inside the home"
+        fi
+else
+  echo "You are a minor."
+fi
--- new.sh
+++ new.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 echo "Enter Your Name"
 read name;
 
